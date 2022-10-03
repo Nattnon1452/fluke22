424a78e7c12f5b0e1827ded87e540cb7364e670a
Nattnon1452 <112382007+Nattnon1452@users.noreply.github.com>
2022-10-03 14:52:13 +0700

    เพิ่มไฟล์ใน repository
    เพิ่มไฟล์ใน repository

---
+++ hotel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include("includes/header.php"); ?>
+</head>
+
+<body>
+<?php include("includes/navbar.php"); ?>
+<div class="container">
+    <!-- Start -->
+<h2>ที่พัก</h2>
+<hr>
+    <!-- End -->
+</div>
+
+</body>
+
+</html>
+<?php include("includes/footer.php"); ?>
